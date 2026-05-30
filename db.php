ebea86d57761a878b60f1beecc6513f8ed24bb82
Haneen Hamdia <haneen421048398@gmail.com>
2026-05-30 14:46:30 +0300

    fix host address

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
-// بيانات الاتصال بقاعدة بيانات Render Postgres الجديدة
-$host = "dpg-cv82m4d2ng1s73eddq0g-a.oregon-postgres.render.com"; 
+// بيانات الاتصال بقاعدة بيانات Render Postgres الفعالة
+$host = "dpg-d8dc6icm0tmc73dpkk8g-a.oregon-postgres.render.com"; 
 $db   = "my_database_t03o";
 $user = "my_database_t03o_user";
-$pass = "1Hi5ev42HGZOLhDiKHPQsDfXOgyZuD31";
+$pass = "1Hi5ev42HGZOLhDiKHPQsDfX0gyZuD31"; 
 $port = "5432";
 
 try {
     // إنشاء الاتصال باستخدام الـ PDO المخصص لـ Postgres
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
-    // تفعيل وضع الأخطاء للمساعدة في المراقبة
+
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("ERROR: Could not connect. " . $e->getMessage());
